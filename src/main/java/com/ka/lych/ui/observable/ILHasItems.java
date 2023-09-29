ff702e0e32c42101e92ad6b6074e0e5d88d32ac7
ahrkla <ahrkla@STAP0015300208M.stadlerrail.ch>
2023-09-29 13:44:40 +0200

    0.01

--- src/main/java/com/ka/lych/ui/observable/ILHasItems.java
+++ src/main/java/com/ka/lych/ui/observable/ILHasItems.java
@@ -8,10 +8,16 @@ import com.ka.lych.util.ILConstants;
  *
  * @author klausahrenberg
  * @param <T>
+ * @param <BC>
  */
-public interface ILHasItems<T> {
+public interface ILHasItems<T, BC> {
     
     public LObject<LList<T>> items();
+    
+    default public BC items(LList<T> items) {
+        items().set(items);
+        return (BC) this;
+    }
     
     public default String getItemsValueName() {
         return ILConstants.ITEMS;
