6c5dcd5c08f211dd4301db8f1141df4655eb0765
ahrkla <ahrkla@STAP0015202777M.stadlerrail.ch>
2025-03-03 17:15:03 +0100

    0.01

--- src/main/java/com/ka/lych/exception/LException.java
+++ src/main/java/com/ka/lych/exception/LException.java
@@ -1,5 +1,6 @@
 package com.ka.lych.exception;
 
+import com.ka.lych.annotation.Json;
 import com.ka.lych.observable.LString;
 import com.ka.lych.util.ILConsumer;
 import java.util.function.Consumer;
@@ -10,6 +11,9 @@ import java.util.function.Consumer;
  */
 public class LException extends Exception {
     
+    @Json
+    protected int _code;
+    
     public LException(final String message, final Object... arguments) {
         this((Throwable) null, message, arguments);
     }
@@ -19,7 +23,8 @@ public class LException extends Exception {
     }
 
     public LException(final Throwable cause, final String message, final Object... arguments) {       
-        super((message != null ? LString.format(message, arguments) : (cause != null ? cause.getMessage() : "")), cause);        
+        super((message != null ? LString.format(message, arguments) : (cause != null ? cause.getMessage() : "")), cause);    
+        _code = 100;
     }    
 
     public static <T> Consumer<T> throwing(ILConsumer<T> throwingConsumer) {
